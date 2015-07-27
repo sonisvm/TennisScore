e4ed220ef5959eac53ed4374b44d635f927e4388
Sonia V Mathew <sonisvm@gmail.com>
2015-07-27 11:14:52 +0530

    added main function which calls getScoreAt()

--- src/TennisGame.java
+++ src/TennisGame.java
@@ -1,4 +1,9 @@
 
 public class TennisGame {
-
+	 
+	 public static void main(String[] args){
+		
+		 TennisScoreBoard scoreBoard = new TennisScoreBoard(args[0]);
+		 System.out.println(TennisScoreBoard.getScoreAt(Integer.parseInt(args[1])));
+	 }
 }
