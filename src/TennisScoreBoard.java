63008fd038964884759631f8b8fb45be51e5a4fc
Shravan Venkataraman <heraclesphaeton@gmail.com>
2015-07-27 11:17:09 +0530

    Made changes to constructor and removed the setInitialScore function coz of redundancy issue.

--- src/TennisScoreBoard.java
+++ src/TennisScoreBoard.java
@@ -8,16 +8,15 @@ public class TennisScoreBoard {
 	private String winningPlayer;
 	public TennisScoreBoard(String gameString)
 	{
-		calculateScore(gameString);
 		player1Score = new int[gameString.length()];
 		player2Score = new int[gameString.length()];
-		setInitialScore();
+		player1Score[0] = 0;
+		player2Score[0] = 0;
+		calculateScore(gameString);
 	}
 	
 	private void setInitialScore()
 	{
-		player1Score[0] = 0;
-		player2Score[0] = 0;
 	}
 	private void calculateScore(String gameString)
 	{
@@ -29,33 +28,14 @@ public class TennisScoreBoard {
 	
 	private void addUpdatedScore(char playerId, int index)
 	{
-		if(gameOver())
+		if(gameOver()){
 			System.out.println("Player "+ winningPlayer + " wins!");
-		if(isADeuce(index-1))
-		{
-			if(gameOver(1))
-			{
-				System.out.println("Game Over")
-			}
-			else
-			{
-				updateAdvantageCounter(playerId, index);
-			}
 		}
 		else
 		{
-			if(gameOver(0))
-			{
-				if(player1Score[index-1]==40)
-				{
-					System.out.println("Player 1 wins");
-				}
-				else
-				{
-					System.out.println("Player 2 wins");
-				}
-			}
+			updateAdvantageCounter(playerId, index);	
 		}
+		
 	}
 	
 }
